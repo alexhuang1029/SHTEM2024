f70c5b83fe5732531a8af674f7ad9ce84a3360da
Alex Huang <alexahuanga1029@gmail.com>
2024-06-17 12:02:03 -0700

    Added completed Fibonaci Sequence homework

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,17 @@
 print('helloworld')
+a = 0
+i, j = 0,1
+
+# Set starting value for Fibonaci Sequence
+if a == 0:
+    print(0)
+    a = 1
+
+# Set recursive pattern starting from the 2nd term
+if a >= 1 and a < 50:
+    while a < 50:
+        # Allows i to take the value of j
+        # At the same time, j becomes the sum of the previous i and itself
+        i, j = j, i+j
+        print(i)
+        a = a+1
